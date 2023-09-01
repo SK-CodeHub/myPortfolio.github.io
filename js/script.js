581dc9550f85b9ab1b3d1159e2f67726d5ea7d80
SK-CodeHub <126589510+SK-CodeHub@users.noreply.github.com>
2023-09-01 11:30:28 +0530

    Initial commit "My Portfolio"

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+// // UPDATED CODE
+
+// // JavaScript for Sticky Navigation Bar
+// let prevScrollPos = window.pageYOffset;
+// const header = document.querySelector('header');
+
+// window.addEventListener('scroll', function () {
+//     const currentScrollPos = window.pageYOffset;
+
+//     if (currentScrollPos > prevScrollPos) {
+//         header.style.transform = 'translateY(-100%)';
+//     } else {
+//         header.style.transform = 'translateY(0)';
+//     }
+
+//     prevScrollPos = currentScrollPos;
+
+//     if (window.scrollY > 50) {
+//         header.classList.add('sticky');
+//     } else {
+//         header.classList.remove('sticky');
+//     }
+// });
